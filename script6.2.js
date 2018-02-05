ff6be925cf12b7b989690c747cce3b832c3a6628
Max <maxim250291@gmail.com>
2018-02-05 13:06:27 +0300

    final

--- script6.2.js
+++ script6.2.js
@@ -1,17 +1,21 @@
 (function() {
-    let elem = document.getElementsByClassName('form'),
-        re_name = /^а-яА-Яa-zA-Z0-9$/,//can be replaced "\w" for English
-        re_email = /^\w*@\w.com$/,
-        login = document.getElementById('#login'),
-        password = document.getElementById('#password'),
-        email = document.getElementById('#email'),
-        err = document.getElementById('.err');
+    let elem = document.getElementsByClassName('form')[0],
+        re_name = /^[а-яА-Яa-zA-Z0-9]+$/,
+        re_email = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+        login = document.getElementById('login'),
+        password = document.getElementById('password'),
+        email = document.getElementById('email'),
+        err = document.getElementsByClassName('err');
 
-    //elem.submit
-    document.getElementById('#sup').onclick = function() {
-    if (!re_name.test(login.value) && !re_name.test(password.value)
-        && !re_email.test(email.value)) {
-        err.display = 'block';
+    elem.onsubmit = function() {
+    if (!re_name.test(login.value) || !re_name.test(password.value) || !re_email.test(email.value)) {
+        for (let i=0; i<=2; i++) {
+            console.log('work');
+            err[i].style.display = 'block';
+        }
     }
 }
 })();
+
+///^а-яА-Яa-zA-Z0-9$/ can be replaced "\w" for English
+///^\w*@\w.com$/
